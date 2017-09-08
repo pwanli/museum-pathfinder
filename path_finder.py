6d80d849b2d017fcaa3756c30dc5efbe484090fc
Juan-Lee Pang <pwanli@gmail.com>
2017-09-08 09:06:51 -0700

    made toy into a class and added comments

--- path_finder.py
+++ path_finder.py
@@ -1,9 +1,10 @@
 import time
 from sense_hat import SenseHat
-sense = SenseHat()
 
+# constants
 MAX_GRID_SIDE_INDEX = 7
 GRID_SIDE_LENGTH = MAX_GRID_SIDE_INDEX+1
+SLOW_DOWN_SECONDS = 0.05
 EMPTY = [0, 0, 0] # black
 CURSOR = [255, 255, 255] # white
 COP = [255, 255, 255] # white
@@ -23,78 +24,97 @@ DEFAULT_GRID = [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY,
                 EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY,
                 EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY]
 
-game_grid = list(DEFAULT_GRID)
-cursor_position = [0, 0]
+class MuseumPathFinder:
 
-def paint_steps(x, y, n):
-    if x - n < 0 and x + n > MAX_GRID_SIDE_INDEX and y - n < 0 and y + n > MAX_GRID_SIDE_INDEX:
-        return
+    def __init__(self):
+        self.sense = SenseHat()
+        self.game_grid = list(DEFAULT_GRID)
+        self.cursor_position = [0, 0]
 
-    start_x = max(x - n, 0)
-    end_x = min(x + n, MAX_GRID_SIDE_INDEX)
-    start_y = max(y - n, 0)
-    end_y = min(y + n, MAX_GRID_SIDE_INDEX)
+    def store_steps(self, x, y, n):
+        if x - n < 0 and x + n > MAX_GRID_SIDE_INDEX and y - n < 0 and y + n > MAX_GRID_SIDE_INDEX:
+            return
 
-    for i_x in range(start_x, end_x+1):
-        for i_y in range(start_y, end_y+1):
-            if i_x == start_x or i_x == end_x or i_y == start_y or i_y == end_y:
-                existing = game_grid[coordinates_to_index(i_x, i_y)]
-                if existing == EMPTY or (existing in STEPS and n - 1 < STEPS.index(existing)):
-                    game_grid[coordinates_to_index(i_x, i_y)] = STEPS[n-1]
-                else:
-                    continue
-                time.sleep(0.05)
-                redraw()
+        # stay in bounds
+        start_x = max(x - n, 0)
+        end_x = min(x + n, MAX_GRID_SIDE_INDEX)
+        start_y = max(y - n, 0)
+        end_y = min(y + n, MAX_GRID_SIDE_INDEX)
 
-    paint_steps(x, y, n + 1)
+        # mark the edges of the region with the color code
+        # for the number of steps from a security guard
+        for i_x in range(start_x, end_x+1):
+            for i_y in range(start_y, end_y+1):
+                if i_x == start_x or i_x == end_x or i_y == start_y or i_y == end_y:
+                    existing = self.game_grid[MuseumPathFinder.coordinates_to_index(i_x, i_y)]
+                    if existing == EMPTY or (existing in STEPS and n - 1 < STEPS.index(existing)):
+                        self.game_grid[MuseumPathFinder.coordinates_to_index(i_x, i_y)] = STEPS[n-1]
+                    else:
+                        continue
+                    # slow down the process for visual effect
+                    time.sleep(SLOW_DOWN_SECONDS)
+                    self.redraw()
 
-def calculate_shortest_paths():
-    for x in range(GRID_SIDE_LENGTH):
-        for y in range(GRID_SIDE_LENGTH):
-            if game_grid[coordinates_to_index(x, y)] == COP:
-                paint_steps(x, y, 1)
+        self.store_steps(x, y, n + 1)
 
-def coordinates_to_index(x, y):
-    return x + y * GRID_SIDE_LENGTH
+    def calculate_shortest_paths(self):
+        # visit each cell and calculate/store path lengths
+        # by color coding when a security guard is found
+        for x in range(GRID_SIDE_LENGTH):
+            for y in range(GRID_SIDE_LENGTH):
+                if self.game_grid[MuseumPathFinder.coordinates_to_index(x, y)] == COP:
+                    self.store_steps(x, y, 1)
 
-def redraw():
-    sense.set_pixels(game_grid)
-    sense.set_pixel(cursor_position[0], cursor_position[1], CURSOR[0], CURSOR[1], CURSOR[2])
+    def coordinates_to_index(x, y):
+        # our game grid is an array vs. matrix to simplify interface with sense hat set_pixels
+        return x + y * GRID_SIDE_LENGTH
 
-held = False
-while True:
-    for event in sense.stick.get_events():
-        print(event.direction, event.action)
-        if event.action == "released":
-            if event.direction == "up" and cursor_position[1] > 0:
-                cursor_position[1] -= 1
-            elif event.direction == "left" and cursor_position[0] > 0:
-                cursor_position[0] -= 1
-            elif event.direction == "down" and cursor_position[1] < MAX_GRID_SIDE_INDEX:
-                cursor_position[1] += 1
-            elif event.direction == "right" and cursor_position[0] < MAX_GRID_SIDE_INDEX:
-                cursor_position[0] += 1
-            elif event.direction == "middle":
-                if held and cursor_position == [0, 0]:
-                    calculate_shortest_paths()
-                elif held:
-                    game_grid = list(DEFAULT_GRID)
-                else:
-                    game_grid[coordinates_to_index(cursor_position[0], cursor_position[1])] = COP
+    def redraw(self):
+        # render current grid and joystick cursor position
+        self.sense.set_pixels(self.game_grid)
+        self.sense.set_pixel(self.cursor_position[0], self.cursor_position[1], CURSOR[0], CURSOR[1], CURSOR[2])
 
-            held = False
-            redraw()
+    def animate_toy(self):
+        held = False
+        # joystick input loop
+        while True:
+            for event in self.sense.stick.get_events():
+                if event.action == "released":
+                    # handle cursor movement
+                    if event.direction == "up" and self.cursor_position[1] > 0:
+                        self.cursor_position[1] -= 1
+                    elif event.direction == "left" and self.cursor_position[0] > 0:
+                        self.cursor_position[0] -= 1
+                    elif event.direction == "down" and self.cursor_position[1] < MAX_GRID_SIDE_INDEX:
+                        self.cursor_position[1] += 1
+                    elif event.direction == "right" and self.cursor_position[0] < MAX_GRID_SIDE_INDEX:
+                        self.cursor_position[0] += 1
+                    # handle run, reset, and mark cop
+                    elif event.direction == "middle":
+                        if held and self.cursor_position == [0, 0]:
+                            self.calculate_shortest_paths()
+                        elif held:
+                            self.game_grid = list(DEFAULT_GRID)
+                        else:
+                            self.game_grid[MuseumPathFinder.coordinates_to_index(self.cursor_position[0], self.cursor_position[1])] = COP
 
-        elif event.action == "held":
-            if event.direction == "up" and cursor_position[1] > 0:
-                cursor_position[1] -= 1
-            elif event.direction == "left" and cursor_position[0] > 0:
-                cursor_position[0] -= 1
-            elif event.direction == "down" and cursor_position[1] < MAX_GRID_SIDE_INDEX:
-                cursor_position[1] += 1
-            elif event.direction == "right" and cursor_position[0] < MAX_GRID_SIDE_INDEX:
-                cursor_position[0] += 1
+                    held = False
+                    self.redraw()
 
-            held = True
+                elif event.action == "held":
+                    # handle held down button cursor movement
+                    if event.direction == "up" and self.cursor_position[1] > 0:
+                        self.cursor_position[1] -= 1
+                    elif event.direction == "left" and self.cursor_position[0] > 0:
+                        self.cursor_position[0] -= 1
+                    elif event.direction == "down" and self.cursor_position[1] < MAX_GRID_SIDE_INDEX:
+                        self.cursor_position[1] += 1
+                    elif event.direction == "right" and self.cursor_position[0] < MAX_GRID_SIDE_INDEX:
+                        self.cursor_position[0] += 1
 
+                    held = True
+
+if __name__ == "__main__":
+    toy = MuseumPathFinder()
+    toy.animate_toy()
 
